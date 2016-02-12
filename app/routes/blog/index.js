5a082bdf9e110934d53fdb24a127d414aa26c33b
Eric Marantette <maraneric@gmail.com>
2016-02-12 07:19:54 -0800

    Add cards to blog index

--- app/routes/blog/index.js
+++ app/routes/blog/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
-        return this.store.findAll('post-summary');
+        return this.store.findAll('post-summary').then(array => array.sortBy('timestamp'));
     },
 });
