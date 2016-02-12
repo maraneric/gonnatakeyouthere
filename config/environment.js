00c036d96ce06a98752cde312b049e37046f4b96
Eric Marantette <maraneric@gmail.com>
2016-02-12 07:48:15 -0800

    Updated config

--- config/environment.js
+++ config/environment.js
@@ -17,7 +17,7 @@ module.exports = function(environment) {
       sessionServiceName: 'session'
     },
     baseURL: '/',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
